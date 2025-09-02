7735724310b6869d0d05e01f8dce3424cc7f2010
Nama Anda <bintangabadisatria@gmail.com>
2025-09-02 15:13:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        fadeUp: {
+          '0%': { opacity: 0, transform: 'translateY(40px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        fadeUp: 'fadeUp 0.8s ease-out forwards',
+      }
+    }
+  }
+}
